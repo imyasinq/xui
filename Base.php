63678ff0aba700136cce655a593fcf4b923e1b99
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2023-06-01 02:38:25 +0330

    Added API Routes

---
+++ Base.php
@@ -0,0 +1,17 @@
+<?php
+class Base {
+    protected $protocol, $hostname, $port, $path;
+    protected $cookie;
+    protected $id;
+    protected $methods = [
+        'login'        => "login",
+        'lists'        => "panel/api/inbounds/list",
+        'list'         => "panel/api/inbounds/get/{id}",
+        'add'          => "panel/api/inbounds/add",
+        'update'       => "panel/api/inbounds/update/{id}",
+        'delete'       => "panel/api/inbounds/del/{id}",
+        'addClient'    => "panel/api/inbounds/addClient",
+        'updateClient' => "panel/api/inbounds/updateClient/{id}"
+    ];
+}
+?>
