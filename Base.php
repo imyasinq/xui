adb33da07799b7a1853b70b9754305cd6b986cbf
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2023-06-01 02:44:43 +0330

    Added Generate UID And Mail Method

--- Base.php
+++ Base.php
@@ -37,5 +37,17 @@ class Base {
     public function sizeConvert($size) {
         return $size * 1024 * 1024 * 1024;
     }
+
+    public function generateId() {
+        $data = random_bytes(16);
+        assert(strlen($data) == 16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+        return vsprintf("%s%s-%s-%s-%s-%s%s%s", str_split(bin2hex($data), 4));
+    }
+
+    public function generateMail() {
+        return substr(str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz'), 1, 8);
+    }
 }
 ?>
