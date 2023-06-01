1d2742b4450456b251de8afd6b5204bfc2eaa6ba
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2023-06-01 16:06:07 +0330

    Added getClients Method

--- App/Options.php
+++ App/Options.php
@@ -3,5 +3,16 @@
 namespace mRYasinQ\App;
 
 trait Options {
+    public function getClients($inbound_id) {
+        $inbound = json_decode($this->inbound($inbound_id), true);
+        if (isset($inbound) && $inbound['success'] == true) {
+            $settings = json_decode($inbound['obj']['settings'], true);
+            $clients  = $settings['clients'];
+            if (count($clients) != 0) {
+                return $clients;
+            }
+        }
+        return false;
+    }
 }
 ?>
