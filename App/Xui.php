4175680d89ff8a5e4fdffc30666f18d1e9d67140
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2023-06-01 13:08:36 +0330

    Added Login Method

--- App/Xui.php
+++ App/Xui.php
@@ -6,5 +6,12 @@ class Xui extends Base {
     public function __construct($protocol, $hostname, $port, $path = "") {
         parent::__construct($protocol, $hostname, $port, $path = "");
     }
+
+    public function login($username, $password) {
+        return $this->command('login', [
+            'username' => $username,
+            'password' => $password
+        ], true);
+    }
 }
 ?>
