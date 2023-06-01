6c1979395458e248a8f66dfe28e226a89be0f99d
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2023-06-01 13:07:56 +0330

    Added Construct Method To Xui

--- App/Xui.php
+++ App/Xui.php
@@ -3,6 +3,8 @@
 namespace mRYasinQ\App;
 
 class Xui extends Base {
-    
+    public function __construct($protocol, $hostname, $port, $path = "") {
+        parent::__construct($protocol, $hostname, $port, $path = "");
+    }
 }
 ?>
