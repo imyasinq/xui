f70fca413aa4683d8e23b36e8f3258a86cecde29
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2023-06-02 18:35:27 +0330

    Added generateVless Method

--- App/Additions.php
+++ App/Additions.php
@@ -3,5 +3,24 @@
 namespace mRYasinQ\App;
 
 trait Additions {
+    public function generateVless($inbound_id, $address = "", $header = "") {
+        $inbound = json_decode($this->inbound($inbound_id), true);
+        if (isset($inbound) && $inbound['success'] == true) {
+            $configs = [];
+            $remark  = $inbound['obj']['remark'];
+            $port    = $inbound['obj']['port'];
+            $address = $address == "" ? $this->hostname : $address;
+            $host    = $header == "" ? "" : "&host={$header}";
+            $clients = json_decode($this->getClients($inbound_id));
+            if ($clients) {
+                foreach ($clients as $client) {
+                    $uid       = $client['id'];
+                    $configs[] = "vless://{$uid}@{$address}:{$port}?encryption=none&security=none&type=tcp&headerType=http{$host}#{$remark}-{$inbound_id}";
+                }
+                return $this->jsonEncode($configs);
+            }
+        }
+        return false;
+    }
 }
 ?>
